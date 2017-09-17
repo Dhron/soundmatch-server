043b672d682a2bf0e6da906bf003699b2bb7b34d
Dhron Joshi <dhron.joshi@uwaterloo.ca>
2017-09-16 23:26:37 -0400

    add preprocessing

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 var MidiConvert = require('midiconvert');
 var LogRoutes = require('express-log-routes');
 var logRoutes = new LogRoutes();
+var freq = require('midi-freq');
 
 const routes = require('express').Router();
 
@@ -12,11 +13,21 @@ var MidiConvert = require('@pioug/MidiConvert');
 
 var get_midi = function(req, res){
 
+var processOutput = function(json){
+    var parts = json.parts[0];
+    for(let item in parts){
+        parts[item].freq = freq(null, parts[item].midiNote);
+    }
+    json.parts[0] = parts;
+    return json;
+}
+
 fs.readFile("happy.mid", "binary", function(err, midiBlob){
     if (!err){
         var midi = MidiConvert.parse(midiBlob)
-        var output = JSON.stringify(midi, null, 4);
-        res.send(output);
+        var output = JSON.parse(JSON.stringify(midi, null, 4));
+        var modOutput = processOutput(output);
+        res.send(modOutput);
     }
 });
 
